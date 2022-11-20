04b0aa796e8498cce70a8c43a4f25f9ad6fc35c7
Hector <hm.bm10613@gmail.com>
2022-11-20 15:16:45 -0600

    Adding more quotes

--- javascript.js
+++ javascript.js
@@ -5,7 +5,13 @@ var quotes = ['Life is what happens to us while we are making other plans.― Al
  'Whether you think you can or think you can\’t, you\’re right. – Henry Ford', 
  'Your talent determines what you can do. Your motivation determines how much you\’re willing to do. Your attitude determines how well you do it. —Lou Holtz',
 'The happiness of your life depends on the quality of your thoughts. – Marcus Aurelius',
-'You are who you are meant to be. Dance as if no one\’s watching. Love as if it\’s all you know. Dream as if you\’ll live forever. Live as if you\’ll die today. James Dean']
+'You are who you are meant to be. Dance as if no one\’s watching. Love as if it\’s all you know. Dream as if you\’ll live forever. Live as if you\’ll die today. James Dean',
+'You do not find the happy life. You make it.— Camilla Eyring Kimball', 
+'It is during our darkest moments that we must focus to see the light. — Aristotle',
+'Develop success from failures. Discouragement and failure are two of the surest stepping stones to success. -Dale Carnegie',
+'Just because it’s what’s done doesn\’t mean it\’s what should be done!- Cinderella', 'It’s no use going back to yesterday because I was a different person then. – Alice, Alice In Wonderland',
+'Learn as if you will live forever, live like you will die tomorrow. — Mahatma Gandhi',
+'All our dreams can come true if we have the courage to pursue them.- Walt Disney']
 
 
 function newQuote() {
